98a9c2645e76c90be60afa53ea4908553928cc9e
Youngrok Song <id2thomas@gmail.com>
2025-01-24 13:47:04 +0900

    [add] queue object id check

--- experiments/2501_concurrency/multithreading_2_queue.py
+++ experiments/2501_concurrency/multithreading_2_queue.py
@@ -1,8 +1,9 @@
 '''
 파이썬 multithreading queue 사용 테스트
 '''
-import threading
+import copy
 from concurrent.futures import ThreadPoolExecutor
+import threading
 from queue import Queue
 import time
 
@@ -50,6 +51,55 @@ def use_executor(queue, num_work: int = 100, num_workers = 3, max_workers=3):
             executor.submit(compute_worker, queue)
     print(f"num work {num_work} with {num_workers} threads {time.time()-start:.3f}")
     
+'''
+# 2. Objects with Queue
+* https://stackoverflow.com/questions/41673522/is-python-multiprocessing-queue-safe-for-object-put
+    * queue doesn't automatically deep copy
+    
+## result
+```
+Object ID in main thread: 4350147936
+Object ID in worker: 4350147936
+
+List Example:
+Object ID in main thread: 4302707392
+Object ID in worker: 4302707392
+[1, 2, 3]
+```
+-> same object
+'''
+class MyObject:
+    def __init__(self, value):
+        self.value = value
+        
+def worker(queue):
+    obj = queue.get()
+    print(f"Object ID in worker: {id(obj)}")
+        
+def worker2(queue):
+    test_list = queue.get()
+    print(f"Object ID in worker: {id(test_list)}")
+    test_list.append(3)
+
+def object_id_check():
+    q = Queue()
+    my_obj = MyObject(42)
+    print(f"Object ID in main thread: {id(my_obj)}")
+    q.put(my_obj)
+
+    thread = threading.Thread(target=worker, args=(q,))
+    thread.start()
+    thread.join()
+
+    
+    test_list = [1,2]
+    print(f"Object ID in main thread: {id(test_list)}")
+    q.put(test_list)
+    
+    thread = threading.Thread(target=worker2, args=(q,))
+    thread.start()
+    thread.join()
+    print(test_list)
 
 if __name__=="__main__":
     queue = Queue()
@@ -57,3 +107,6 @@ if __name__=="__main__":
     print("1----use ThreadPoolExecutor")
     use_executor(queue, num_work=200, num_workers=1, max_workers=5)
     use_executor(queue, num_work=200, num_workers=5, max_workers=5)
+    
+    print("2----Queue Object ID")
+    object_id_check()
